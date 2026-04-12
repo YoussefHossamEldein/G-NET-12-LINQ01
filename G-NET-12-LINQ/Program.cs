fd4a29fae106b4370a4cfca041d0eace56048a27
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-12 19:00:59 +0200

    Add project files.

---
+++ G-NET-12-LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace G_NET_12_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+
+
+            #region Question01
+            #endregion
+
+
+        }
+    }
+}
