892071d30c5dc138511f9f9d34aa3e55cf24e233
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-12 19:09:51 +0200

    Question01

--- G-NET-12-LINQ/Program.cs
+++ G-NET-12-LINQ/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace G_NET_12_LINQ
+﻿using LINQ.DataSources;
+using static LINQ.DataSources.Source;
+
+
+namespace G_NET_12_LINQ
 {
     internal class Program
     {
@@ -9,6 +13,56 @@
 
 
             #region Question01
+            //var result = ProductList.Where(p => p.Category == "Seafood").Select(n => new { n.ProductName, n.UnitPrice, n.Category });
+
+            //var result = from p in ProductList
+            //             where p.Category == "Seafood"
+            //             select new { p.ProductName, p.UnitPrice };
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            #endregion
+
+            #region Question02
+
+
+            #endregion
+
+            #region Question03
+            #endregion
+
+            #region Question04
+            #endregion
+
+            #region Question05
+            #endregion
+
+            #region Question06
+            #endregion
+
+            #region Question07
+            #endregion
+
+            #region Question08
+            #endregion
+
+            #region Question09
+            #endregion
+
+            #region Question10
+            #endregion
+
+            #region Question11
+            #endregion
+
+            #region Question12
+            #endregion
+
+            #region Question13
             #endregion
 
 
