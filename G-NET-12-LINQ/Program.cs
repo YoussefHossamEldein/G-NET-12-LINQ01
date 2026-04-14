338bacba32c911ca0585e0d83904d832d69299ad
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-14 12:06:23 +0200

    FirstFiveQuestions

--- G-NET-12-LINQ/Program.cs
+++ G-NET-12-LINQ/Program.cs
@@ -19,29 +19,83 @@ namespace G_NET_12_LINQ
             //             where p.Category == "Seafood"
             //             select new { p.ProductName, p.UnitPrice };
 
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
+
 
 
             #endregion
 
             #region Question02
-
-
+            //var result = ProductList.Select(p => p.ProductName);
             #endregion
 
             #region Question03
+            //var result = from p in ProductList
+            //             orderby p.UnitPrice ascending
+            //             select new { p.ProductName, p.UnitPrice };
+
+            //var result = ProductList.OrderBy(p => p.UnitPrice).Select(p => new { p.ProductName, p.UnitPrice });
+
+
             #endregion
 
             #region Question04
+
+            //var result = ProductList.Where(p => p.UnitPrice > 10 && p.UnitPrice < 20);
+
+
             #endregion
 
             #region Question05
+
+            //var result = ProductList.Where(p => p.UnitsInStock > 0 && p.Category == "Condiments");
+            //var result = from p in ProductList
+            //             where p.UnitsInStock > 0 && p.Category == "Condiments"
+            //             select p;
+
+
+
+
             #endregion
 
             #region Question06
+
+            //var result = from p in ProductList
+            //             where p.UnitsInStock > 0 && p.Category == "Condiments"
+            //             select p.UnitPrice;
+
+
+            //var result = ProductList.Take(5);
+
+            //var result = ProductList.Skip(10).Take(10);
+            int[] numbers = { 5, 4, 3, 1, 6, 9, 2, 8 };
+
+            //var result = numbers.TakeWhile(n => n < 6);
+
+            //var result = numbers.TakeWhile((n, i) => n >= i);
+
+            //var result = numbers.TakeWhile(n => n % 3 == 0);
+            //var result = ProductList.Any(n => n.UnitsInStock == 0);
+
+
+            //var result = ProductList.Where(n => n.Category == "Seafood").All(n => n.UnitsInStock > 0);
+            //Console.WriteLine(result);
+
+
+            var result = from p in ProductList
+                         let DiscountPrice = p.UnitPrice * 0.5M
+                         where DiscountPrice < 10
+                         select new
+                         {
+                             p.ProductName,
+                             p.UnitPrice,
+                             PriceAfterDiscount = p.UnitPrice * 0.5M
+                         };
+
+            
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
             #endregion
 
             #region Question07
